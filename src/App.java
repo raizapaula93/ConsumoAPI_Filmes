900bac88caab862926b06145c19e1aba73712a77
raizapaula93 <raizapaula93@gmail.com>
2022-07-18 23:27:07 -0300

    Projeto video - consumo de api em video IMDB

---
+++ src/App.java
@@ -0,0 +1,38 @@
+import javax.swing.plaf.basic.BasicSliderUI;
+import java.net.URI;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+
+import static java.net.http.HttpClient.newHttpClient;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println();
+        //Fazer uma conexão HTTP e buscar top 250 filmes
+
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI endereco = URI.create(url);
+        var client = newHttpClient(); //a IDE conseguiu adivinhar o tipo
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        String body = response.body();
+        //System.out.println(body);
+        //Extrair o título,poster,classificação(parsear)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+        //System.out.println(listaDeFilmes.size());
+        //Exibir e manipular os dados
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
