5fce59b4e25b99f31171fc27c700d6155b9a267c
raizapaula93 <raizapaula93@gmail.com>
2022-07-20 18:04:01 -0300

    Projeto gerador de figurinhas

--- src/App.java
+++ src/App.java
@@ -1,15 +1,10 @@
-import javax.swing.plaf.basic.BasicSliderUI;
 import java.io.InputStream;
 import java.net.URI;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
-
 
 import static java.net.http.HttpClient.newHttpClient;
 
